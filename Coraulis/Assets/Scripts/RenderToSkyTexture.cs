bc0769f50afc875a0b31bfc3ff46154aae724473
Etienne Peillard <etienne.peillard@ec-nantes.fr>
2019-04-29 18:17:39 +0200

    New FBX Better screen projection

---
+++ Coraulis/Assets/Scripts/RenderToSkyTexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RenderToSkyTexture : MonoBehaviour
+{
+    Camera cam;
+    public RenderTexture toRender;
+    public RenderTexture tempCubeMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.enabled = true;
+        cam.RenderToCubemap(tempCubeMap);
+        tempCubeMap.ConvertToEquirect(toRender);
+        cam.enabled = false;
+    }
+}
