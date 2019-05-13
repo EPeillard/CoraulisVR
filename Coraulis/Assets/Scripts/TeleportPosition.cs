156682d1a631384556fa35451243cbc100ae808e
Etienne Peillard <etienne.peillard@ec-nantes.fr>
2019-05-13 11:39:00 +0200

    Add tp

---
+++ Coraulis/Assets/Scripts/TeleportPosition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPosition : MonoBehaviour
+{
+    public GameObject steamVrRef;
+    public List<GameObject> positions;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = steamVrRef.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            steamVrRef.transform.position = startPos;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            steamVrRef.transform.position = positions[0].transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            steamVrRef.transform.position = positions[1].transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            steamVrRef.transform.position = positions[2].transform.position;
+        }
+    }
+}
