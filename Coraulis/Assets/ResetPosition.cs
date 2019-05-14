385d293e734aa1e09dac82b7ae9e6691040f5e03
Etienne Peillard <etienne.peillard@ec-nantes.fr>
2019-05-14 23:53:29 +0200

    Some fixes (mirror mat, epicam layer, reset red sphere position)

---
+++ Coraulis/Assets/ResetPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    private Vector3 oldPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.transform.position = oldPos;
+        }
+    }
+}
